bab4b30d2305d803eecdcf30d02f08cfa3d99fd7
Jacopo Bufalino <jacopobufalino@jackops.local>
2025-03-28 17:27:17 +0100

    Bump version: 0.1.15 → 0.1.16

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read_requirements(filename="requirements.txt"):
 
 setup(
     name='orca',
-    version='0.1.15',
+    version='0.1.16',
     packages=find_packages(),
     install_requires=read_requirements(),
     entry_points={
